650194e69d06c43b2374a0e94c964f789407d92e
Pierre Larghero <pierre.larghero@hei.yncrea.fr>
2020-09-01 10:25:40 +0200

    test

---
+++ src/main/java/Drug.java
@@ -0,0 +1,13 @@
+public class Drug {
+
+    private String name;
+    private String lab;
+
+    public String getLab() {
+        return lab;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
